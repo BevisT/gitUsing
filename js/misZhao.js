cc5999900cf04d76395681a25eaabb8ceeaa1ce0
BevisT <BevisYu299@163.com>
2018-07-18 17:15:13 +0800

    百度的搜索界面

--- js/misZhao.js
+++ js/misZhao.js
@@ -1,3 +1,5 @@
-$(".misZhao").on ("click",function(){
-    alert("赵雅美是美女");
-})
+console.log($(".search").outerHeight()*-1*0.5);
+console.log($(".search").outerWidth()*(-1)*0.5);   
+ $(".search").css({"margin-top":$(".search").outerHeight()*(-1)*2,"margin-left":$(".search").outerWidth()*(-1)*0.5}); 
+
+  
